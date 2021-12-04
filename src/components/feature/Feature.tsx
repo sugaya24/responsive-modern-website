c748f4bb964a0dcb18e93110a1221608e39736b5
YukiSugaya <sugaya324@gmail.com>
2021-12-05 05:01:32 +0900

    ADD: whatGPT3 section

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = () => {
-  return <div>Feature</div>;
+type Props = {
+  title: string;
+  text: string;
+};
+
+const Feature = (props: Props) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{props.title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{props.text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
--- src/containers/whatGPT3/WhatGPT3.tsx
+++ src/containers/whatGPT3/WhatGPT3.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatGPT3.css';
 
+const featuresData = [
+  {
+    title: 'What is GPT-3',
+    text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.',
+  },
+  {
+    title: 'Chatbots',
+    text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. ',
+  },
+  {
+    title: 'Knowledgebase',
+    text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b',
+  },
+  {
+    title: 'Education',
+    text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b',
+  },
+];
+
 const WhatGPT3 = () => {
-  return <div>whatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title={featuresData[0].title} text={featuresData[0].text} />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        {featuresData.splice(1, 3).map((data) => (
+          <Feature title={data.title} text={data.text} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
