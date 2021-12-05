981e8178c7044a0fd3b939faca52709aadba4625
YukiSugaya <sugaya324@gmail.com>
2021-12-06 00:22:57 +0900

    ADD: blog

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './article.css';
 
-const Article = () => {
-  return <div>Article</div>;
+type ArticleProps = {
+  imgUrl: string;
+  date: string;
+  title: string;
+};
+
+const Article: FC<ArticleProps> = ({ imgUrl, date, title }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
